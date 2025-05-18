28b76dae09496b057f2509cc1e2fdb03808fdeb0
Jeff Monreal <jeffmonreal1@gmail.com>
2025-05-18 01:02:20 -1200

    DATABASE CHANGED

--- Node.js + MySQL - Boilerplate API/src/_helpers/db.js
+++ Node.js + MySQL - Boilerplate API/src/_helpers/db.js
@@ -1,4 +1,4 @@
-const config = require('config.json');
+const config = require('../../config.json');
 const mysql = require('mysql2/promise');
 const { Sequelize } = require('sequelize');
 
@@ -9,11 +9,15 @@ initialize();
 async function initialize() {
   // create db if it doesn't already exist
   const { host, port, user, password, database } = config.database;
-  const connection = await mysql.createConnection({ host, user, password });
+  const connection = await mysql.createConnection({ host, user, password, port });
   await connection.query(`CREATE DATABASE IF NOT EXISTS \`${database}\`;`);
 
   // connect to db
-  const sequelize = new Sequelize(database, user, password, { dialect: 'mysql' });
+   const sequelize = new Sequelize(database, user, password, {
+    host,      
+    port,     
+    dialect: 'mysql'
+  });
 
   // init models and add them to the exported db object
   db.Account = require('../_accounts/account.model')(sequelize);
