eedcf2ca02fc17d153e53fae7e9d83ccd634ebf4
Jeff Monreal <jeffmonreal1@gmail.com>
2025-05-18 17:00:48 -1200

    NEW MAY 18, 2025

--- Frontend/src/environments/environment.prod.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: true,
-  apiUrl: 'https://4000-firebase-user-management-system-1747518362589.cluster-zumahodzirciuujpqvsniawo3o.cloudworkstations.dev'
-};
-  
---
+++ Node.js + MySQL - Boilerplate API/server.js
@@ -0,0 +1,39 @@
+require('rootpath')();
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
+const errorHandler = require('./src/_middleware/error-handler');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(cookieParser());
+
+// allow cors requests from any origin and with credentials
+app.use(cors({ origin: (origin, callback) => callback(null, true), credentials: true }));
+
+// api routes
+app.use('/accounts', require('./src/_accounts/accounts.controller'));
+
+// swagger docs route
+app.use('/api-docs', require('./src/_helpers/swagger'));
+
+// departments routes
+app.use('/departments', require('./src/departments'));
+
+// employees routes
+app.use('/employees', require('./src/employees'));
+
+// requests routes
+app.use('/requests', require('./src/request'));
+
+// workflows routes
+app.use('/workflows', require('./src/workflows'));
+
+// global error handler
+app.use(errorHandler);
+
+// start server
+const port = process.env.NODE_ENV === 'production' ? (process.env.PORT || 80) : 4000;
+app.listen(port, () => console.log('Server listening on port ' + port));
